05be8a319e3fd873c9281f0da83a8bce110a3b44
Pavlo <pavdemchnko@gmail.com>
2024-12-22 16:49:42 -0500

    Added basic calculator without division by zero

---
+++ eduHillel_HW4.2_Calculator/eduHillel_HW4.2_Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Hillel_HW42_Demchenko
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            int resultOperation = 0;
+            char operation;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the first number:");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number:");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the operation:");
+                operation = Convert.ToChar(Console.ReadLine());
+                switch (operation)
+                {
+                    case '+':
+                        resultOperation = firstNumber + secondNumber;
+                        break;
+                    case '-':
+                        resultOperation = firstNumber - secondNumber;
+                        break;
+                    case '*':
+                        resultOperation = firstNumber * secondNumber;
+                        break;
+                    case '/':
+                        resultOperation = firstNumber / secondNumber;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation");
+                        break;
+
+
+                }
+                Console.WriteLine($"Result: {resultOperation}");
+                Console.WriteLine("Press any key to continue or close the console");
+                Console.ReadKey();
+
+
+            }
+
+        }
+
+    }
+}
